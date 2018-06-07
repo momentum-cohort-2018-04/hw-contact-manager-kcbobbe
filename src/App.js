2d1c9a45b19dbd1d807c25dfd5f70d166d316838
kcbobbe <kcbobbe@gmail.com>
2018-06-07 08:55:40 -0400

    final commit for thursday morning

--- src/App.js
+++ src/App.js
@@ -7,86 +7,85 @@ import uuid from 'uuid/v4'
 /* global localStorage */
 
 class App extends Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       contacts: [],
       username: null,
       password: null,
       loggedOut: true,
-      addContact:false,
-      contactName:null,
-      contactCity:null
+      addContact: false,
+      contactName: null,
+      contactCity: null
     }
     // this.logout = this.logout.bind(this)
-    this.login = this.login.bind(this)
     console.log(this.state);
     // localStorage.loggedOut = 0
   }
-    render() {
-      return (
+  render() {
+    return (
+      <div>
+    {
+        this.state.loggedOut ?(
+          <div>
+          {/* <h1>logged out</h1> */}
+          <div className = "App">
+          <h1>THE CONTACT MANAGER</h1>
+            <div className = "input-field">
+              <label>Username: </label>
+              <input type='text' onChange = {this.username.bind(this)}/>
+            </div>
+  
+            <div className="input-field">
+              <label>Password: </label>
+              <input type="password" onChange = {this.password.bind(this)}/>
+            </div>   
+                  
+            <button type="button" className="button" onClick = {this.showContacts.bind(this)}>Submit!</button>
+          </div>
+          </div>
+        ) :
+        this.state.addContact ?(
         <div>
-        {
-          this.state.loggedOut ?(
-            <div>
-            {/* <h1>logged out</h1> */}
-            <div className = "App">
-            <h1>THE CONTACT MANAGER</h1>
-              <div className = "input-field">
-                <label>Username: </label>
-                <input type='text' onChange = {this.username.bind(this)}/>
-              </div>
-    
-              <div className="input-field">
-                <label>Password: </label>
-                <input type="password" onChange = {this.password.bind(this)}/>
-              </div>   
-                    
-              <button type="button" className="button" onClick = {this.showContacts.bind(this)}>Submit!</button>
-            </div>
-            </div>
-          ) :
-          this.state.addContact ?(
-          <div>
-            <h1>Add a Contact!</h1>
-          <div className = "input-field">
-            <label>Name</label>
-            <input type = 'text' onChange = {this.contactName.bind(this)} />
-          </div>
-          <div className = "input-field">
-            <label>City</label>
-            <input type= 'text' onChange = {this.contactCity.bind(this)}/>
-            <button type = "button" onClick = {this.newContact.bind(this)}>Submit!</button>
-            <button type = "button" onClick = {this.cancelAdd.bind(this)}>Cancel</button>
-
-          </div>
-          </div>
-        ) 
-        
-         : (
-          <div>
-           <div className = "input-field">
-            <label>Search for Contacts</label>
-            <input type = 'text'/>
-          </div>
-          <div className = "header">
-          <button type = "button" className="button" onClick = {this.logout.bind(this)}>LOGOUT</button>
-          <button type="button" className="button" onClick = {this.clickNewContact.bind(this)}>New Contact!</button>
-          </div>
-         
-            {this.state.contacts.map(function(contact){
-              return <Contacts contactData = {contact}
-              // delete={this.deleteContact}
-              />
-              
-            })}
-           
-          </div>
-        )}
-          {/*end else statement */}
+          <h1>Add a Contact!</h1>
+        <div className = "input-field">
+          <label>Name</label>
+          <input type = 'text' onChange = {this.contactName.bind(this)} />
         </div>
+        <div className = "input-field">
+          <label>City</label>
+          <input type= 'text' onChange = {this.contactCity.bind(this)}/>
+          <button type = "button" onClick = {this.newContact.bind(this)}>Submit!</button>
+          <button type = "button" onClick = {this.cancelAdd.bind(this)}>Cancel</button>
 
+        </div>
+        </div>
+      ) 
+      
+        : (
+        <div>
+          <div className = "input-field">
+          <label>Search for Contacts</label>
+          <input type = 'text'/>
+        </div>
+        <div className = "header">
+        <button type = "button" className="button" onClick = {this.logout.bind(this)}>LOGOUT</button>
+        <button type="button" className="button" onClick = {this.clickNewContact.bind(this)}>New Contact!</button>
+        </div>
+        
+          {this.state.contacts.map(function(contact){
+            return <Contacts contactData = {contact}
+            // delete={this.deleteContact}
+            />
+            
+          })}
+          
+        </div>
       )}
+        {/*end else statement */}
+      </div>
+
+    )}
   // componentDidMount () {
   //   request.get('http://localhost:8000/contacts')
   //   . auth(this.state.username,this.state.password)
@@ -102,63 +101,61 @@ class App extends Component {
   //     })
   //   })
   // }
-  login() {
 
-  }
   showContacts () {
-  
     request.get('http://localhost:8000/contacts')
-    .auth(this.state.username,this.state.password)
-    .then((res) => {
-      console.log("LOGGED IN")
-      this.setState({loggedOut:false})
-      this.setState({contacts : res.body})
-      console.log(this.state.loggedOut)
-      localStorage.loggedOut = false
-    })
-  
-    }
-  
-  clickNewContact (){
+      .auth(this.state.username,this.state.password)
+      .then((res) => {
+        console.log("LOGGED IN")
+        this.setState({loggedOut:false})
+        this.setState({contacts : res.body})
+        console.log(this.state.loggedOut)
+        localStorage.loggedOut = false
+      })
+  }
+
+  clickNewContact () {
     this.setState({
-      addContact:true
+      addContact: true
     })
   }
 
-  contactName(event){
+  contactName(event) {
     this.setState({
       contactName: event.target.value
     })
   }
 
-  contactCity(event){
+  contactCity(event) {
     this.setState({
       contactCity: event.target.value
     })
   }
-  
+
   newContact () {
     const newId = uuid()
     request.post('http://localhost:8000/contacts')
-    .auth(this.state.username,this.state.password)
-    .send({
-      "id" : newId,
-      "name" : this.state.contactName,
-      "city": this.state.contactCity,
-    })
-    .then((res)=> {
-      this.setState({
-        addContact:false,
-        contactName:'null',
-        contactCity:'null'
+      .auth(this.state.username,this.state.password)
+      .send({
+        "id" : newId,
+        "name" : this.state.contactName,
+        "city": this.state.contactCity,
       })
-      this.showContacts()
-    })
+      .then((res)=> {
+        this.setState({
+          addContact: false,
+          contactName: 'null',
+          contactCity: 'null'
+        })
+        this.setState({loggedOut:false})
+
+        this.showContacts()
+      })
   }
 
-  cancelAdd(){
+  cancelAdd() {
     this.setState({
-      addContact:false
+      addContact: false
     })
   }
   // deleteContact(){
@@ -167,23 +164,23 @@ class App extends Component {
   //   .then((res)=> {
   //     this.showContacts()
   //   })
-  //   }
-  
-  username(event) {
+  //   } 
+
+  username (event) {
     this.setState({
       username: event.target.value
     })
     localStorage.username = event.target.value
   }
 
-  password(event){
+  password (event) {
     this.setState({
       password: event.target.value
     })
     localStorage.password = event.target.value
   }
 
-  logout(event){
+  logout (event) {
     this.setState({
       loggedOut: true,
       username:null,
@@ -192,97 +189,89 @@ class App extends Component {
     localStorage.loggedOut = true
     localStorage.username = null
     localStorage.password = null
-    console.log(this.state)
-    
+    console.log(this.state) 
   }
 
   componentDidMount () {
-    console.log('heyy')
     request.get('http://localhost:8000/contacts')
-    .auth(localStorage.username,localStorage.password)
-    .then((res) => {
-      console.log("woot")
-      this.setState({loggedOut:false})
-      this.setState({contacts : res.body})
-      console.log(this.state.loggedOut)
-      console.log('component DID mount')
-      localStorage.loggedOut = false
-    })
-  
-
+      .auth(localStorage.username,localStorage.password)
+      .then((res) => {
+      // this.setState({loggedOut:false})
+        this.setState({contacts : res.body})
+        console.log(this.state.loggedOut)
+        localStorage.loggedOut = false
+        this.setState({loggedOut: false})
+      })
 
     this.setState({
       username: localStorage.username,
-      password: localStorage.password,
+      password: localStorage.password
       // loggedOut: localStorage.loggedOut,
-      })
-      console.log("logged in")
-
-    
-  }
-
-}
-
-class ContactContainer extends Component {
-  constructor() {
-    super()
-  }
-  componentDidMount(){
-    
+    })
+    console.log("logged in")
   }
 }
+
+// class ContactContainer extends Component {
+//   constructor() {
+//     super()
+//   }
+//   componentDidMount(){
+    
+//   }
+// }
 
 class Contacts extends Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       contacts: [],
-      editContact : false,
-      editedName : 'null',
-      editedCity : 'null',
+      editContact: false,
+      editedName: 'null',
+      editedCity: 'null'
     }
   }
 
-  deleteContact(){
+  deleteContact () {
     const id = this.props.contactData.id
     request.delete('http://localhost:8000/contacts/'+id)
-    .auth(localStorage.username,localStorage.password)
-     .then((res)=> {
-     })
-    }
-  
-    clickEditContact (){
-      this.setState({
-        editContact : true
+      .auth(localStorage.username,localStorage.password)
+      .then((res)=> {
       })
-    }
-  
-    editedName(event){
-      this.setState({
-        editedName: event.target.value
-      })
-    }
-  
-    editedCity(event){
-      this.setState({
-        editedCity: event.target.value
-      })
-    }
+  }
+
+  clickEditContact () {
+    this.setState({
+      editContact : true
+    })
+  }
+
+  editedName(event) {
+    this.setState({
+      editedName: event.target.value
+    })
+  }
+
+  editedCity(event) {
+    this.setState({
+    editedCity: event.target.value
+    })
+  }
   
 
-  editContact(){
+  editContact() {
     const id = this.props.contactData.id
     request.put('http://localhost:8000/contacts/'+id)
-    .auth(localStorage.username,localStorage.password)
-    .send({
-    "name" : this.state.editedName,
-    "city": this.state.editedCity})
-    .then((res)=> {
-    console.log('edited')
-    this.setState({
-      editContact:false,
-      contactName:'null',
-      contactCity:'null'
+      .auth(localStorage.username,localStorage.password)
+      .send({
+        "name" : this.state.editedName,
+        "city": this.state.editedCity})
+      .then((res)=> {
+       console.log('edited')
+       this.setState({
+          editContact:false,
+          contactName:'null',
+          contactCity:'null'
     })
     // this.showContacts()
   })
